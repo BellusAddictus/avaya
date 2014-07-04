4fc141b1331eaf191a7582439be1f460cd16a6b5
bellus <bellus.addictus@gmail.com>
2014-07-04 23:28:39 +0400

    db-sqlite

---
+++ src/db/Sqlite.java
@@ -0,0 +1,44 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Sqlite
+{
+    public static void main(String[] args) throws ClassNotFoundException
+    {
+
+        Class.forName("org.sqlite.JDBC");
+
+        Connection connection = null;
+        try
+        {
+
+            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+
+
+        }
+        catch(SQLException e)
+        {
+
+            System.err.println(e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if(connection != null)
+                    connection.close();
+            }
+            catch(SQLException e)
+            {
+                // connection close failed.
+                System.err.println(e);
+            }
+        }
+    }
+}
