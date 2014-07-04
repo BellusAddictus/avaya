279712ac04f931b800dc2b16d2c3dbbb4069f6bc
smBG <ganinaa@gmail.com>
2014-07-05 00:57:29 +0400

    edit get.File.java edit copyright

--- src/get/File.java
+++ src/get/File.java
@@ -1,12 +1,14 @@
+/*
+ * A.Ganin 2014.
+ */
+
+
 package get;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-/**
- * Created by gaa on 04.07.14.
- */
 public class File {
     public File(String file) throws IOException {
         ReadFile(file);
@@ -18,34 +20,21 @@ public class File {
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
             while (line != null) {
+/*                sb.append(line);
+                sb.append(System.lineSeparator());
+                String all = sb.toString();*/
                 line = br.readLine();
-                String all = sb.toString();
-
                 if (line.length() > 40) {
                     line = ClearString(line);
-                    String[] word = line.split(";");
-                    System.out.print(word[0]);
-                    System.out.print(word[1]);
-                    System.out.print(word[2]);
-                    System.out.print(word[3]);
-                    System.out.print(word[4]);
-                    System.out.print(word[5]);
-                    System.out.println(word[6]);
-//                    System.out.println(word[7]);
-/*                    if (word[7].isEmpty()) {
-                        System.out.println("Не хватает данных");
-//                        System.out.println(word[7]);
+                    String word[] = line.split(";");
+                    if (word.length < 8) {
+                        System.out.println("False");
                     } else {
-//                        Sqlite.fill();
+                        System.out.println("OK");
+                    }
 
-                    }*/
-
-//                    System.out.println(line);
                 }
-
-
             }
-
 
         } finally {
             br.close();
