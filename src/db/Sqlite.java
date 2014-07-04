f03df0e93cd7b0bdd6793f56953c6046397dbc9c
bellus <bellus.addictus@gmail.com>
2014-07-05 00:56:33 +0400

    db-sqlite

--- src/db/Sqlite.java
+++ src/db/Sqlite.java
@@ -1,32 +1,47 @@
 package db;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 
 public class Sqlite
 {
-    public static void main(String[] args) {
-        try {
-            Class.forName("org.sqlite.JDBC");
-        }
-        catch  (ClassNotFoundException e)    {  }
-
-        Connection connection = null;
+    public static void connect(Connection connection)
+    {
         try {
 
             connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);
-             }
+
+        }
         catch (SQLException e) {}
 
     }
 
-
-    public static void fill()
+    public static void checkbd(Statement statement)
     {
+        try {
+            ResultSet rs = statement.executeQuery("SELECT * FROM tablename");
+            while (rs.next())
+            {
+
+
+                String s = rs.getString("b");
+
+
+            }
+        }
+        catch (SQLException e){}
+    }
+
+
+
+    public static void fill(Statement statement, String s1, String s2, String s3, String s4, String s5, String s6, String s7, String s8)
+    {
+        try {
+        statement.executeUpdate("")
+        }
+        catch (SQLException e){}
+
 
 
     }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,10 @@
 import get.File;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by gaa on 04.07.14.
@@ -9,5 +13,13 @@ public class main {
     public static void main(String[] args) throws IOException {
         String file = "/tmp/Avaya.log";
         new File(file);
+        try {
+            Class.forName("org.sqlite.JDBC");
+        }
+        catch  (ClassNotFoundException e)    {  }
+
+        Connection connection = null;
+
+
     }
 }
