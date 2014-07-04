a7664b8559670c543a1ce96abcdc7446f420f1e3
smBG <ganinaa@gmail.com>
2014-07-05 00:19:41 +0400

    edit main.java edit get.File.java

--- src/db/Sqlite.java
+++ src/db/Sqlite.java
@@ -1,26 +1,26 @@
 package db;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 
-public class Sqlite
-{
-    public static void connect() throws ClassNotFoundException
-    {
-       Class.forName("org.sqlite.JDBC");
+public class Sqlite {
+    public static void connect() throws ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
 
         Connection connection = null;
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            }
-        catch (SQLException e) {System.err.println(e.getMessage());}
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
     }
-    public static void fill()
-    {
+
+    public static void fill() {
 
 
     }
