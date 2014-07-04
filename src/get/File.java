d23f6147de98fa3874e8c62cc452bd09de700d0e
smBG <ganinaa@gmail.com>
2014-07-05 01:37:31 +0400

    edit get.File.java edit copyright

--- src/get/File.java
+++ src/get/File.java
@@ -28,11 +28,10 @@ public class File {
                     line = ClearString(line);
                     String word[] = line.split(";");
                     if (word.length < 8) {
-                        System.out.println("False");
+                        Sqlite.fill(word[0],word[1],word[2],word[3],word[4],word[5],word[6],
                     } else {
                         System.out.println("OK");
                     }
-
                 }
             }
 
--- src/main.java
+++ src/main.java
@@ -2,9 +2,6 @@ import get.File;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * Created by gaa on 04.07.14.
@@ -13,10 +10,11 @@ public class main {
     public static void main(String[] args) throws IOException {
         String file = "/tmp/Avaya.log";
         new File(file);
+
         try {
             Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
         }
-        catch  (ClassNotFoundException e)    {  }
 
         Connection connection = null;
 
