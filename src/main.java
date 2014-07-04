4bb9dc3107aa9ec91e81d054c4a93df964b18a87
bellus <bellus.addictus@gmail.com>
2014-07-05 01:35:07 +0400

    db-sqlite

--- src/main.java
+++ src/main.java
@@ -2,9 +2,7 @@ import get.File;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+
 
 /**
  * Created by gaa on 04.07.14.
@@ -12,14 +10,14 @@ import java.sql.Statement;
 public class main {
     public static void main(String[] args) throws IOException {
         String file = "/tmp/Avaya.log";
-        new File(file);
+
         try {
             Class.forName("org.sqlite.JDBC");
         }
         catch  (ClassNotFoundException e)    {  }
 
         Connection connection = null;
-
+        new File(file);
 
     }
 }
