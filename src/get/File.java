99ffc3937fd2a31f2e46fd2caa438f8cc9ffc4fb
smBG <ganinaa@gmail.com>
2014-07-04 23:41:54 +0400

    add main.java add get.File.java

--- src/get/File.java
+++ src/get/File.java
@@ -1,7 +1,40 @@
 package get;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by gaa on 04.07.14.
  */
 public class File {
+    public static void ReadFile(String file) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                line = br.readLine();
+                String all = sb.toString();
+
+                if (line.length() > 40) {
+                    line = ClearString(line);
+                    System.out.println(line);
+                }
+
+
+            }
+
+
+        } finally {
+            br.close();
+        }
+
+    }
+
+    public static String ClearString(String line) {
+        String clearLine;
+        clearLine = line.replaceAll(" +", ";");
+        return clearLine;
+    }
 }
---
+++ src/main.java
@@ -0,0 +1,7 @@
+/**
+ * Created by gaa on 04.07.14.
+ */
+public class main {
+    public static void main(String[] args) {
+    }
+}
